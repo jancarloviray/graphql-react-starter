e393e86108f74ce39ef4369f7f14ec97c6ae59a8
jancarloviray <virayjancarlo@yahoo.com>
2016-12-29 10:57:26 -0800

    feat(schema): added defaults and non null attr

--- src/db/migrations/20161228171145_initial-schema.js
+++ src/db/migrations/20161228171145_initial-schema.js
@@ -3,13 +3,13 @@ exports.up = function (knex, Promise) {
     return Promise.all([
         knex.schema.createTableIfNotExists('authors', (table) => {
             table.increments('id').primary()
-            table.text('firstName')
-            table.text('lastName')
+            table.text('firstName').notNullable()
+            table.text('lastName').notNullable()
         }),
         knex.schema.createTableIfNotExists('posts', (table) => {
             table.increments('id').primary()
-            table.text('title')
-            table.integer('voteCount')
+            table.text('title').notNullable()
+            table.integer('voteCount').defaultTo(0)
             table.integer('authorId').references('author.id')
         })
     ]).catch(reason => console.log(reason))
